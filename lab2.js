9b697cfac76d7e95762a72475b6c059cf4e106d5
Hoang <taovohoang2k6@gmail.com>
2025-11-05 15:56:19 +0700

    lab2

--- lab2.js
+++ lab2.js
@@ -20,3 +20,37 @@ console.log(student)
 // 1. Thêm thuộc tính grade với giá trị "A"
 // 2. Thay đổi tuổi thành 21
 // 3. In ra toàn bộ thông tin student
+// Tạo template cho email thông báo
+const user = {
+  firstName: "Nguyen",
+  lastName: "Van A",
+  product: "Laptop Dell XPS",
+  price: 25000000,
+  orderDate: "2024-01-15",
+};
+
+// Tạo template string cho email
+const emailTemplate = `
+Xin chào ${user.firstName} ${user.lastName}
+Sản phẩm ${user.product}
+Giá ${user.price}
+Date ${user.orderDate}
+`; // Viết template ở đây
+
+console.log(emailTemplate);
+// Viết lại object sau sử dụng ES6 enhanced object literals
+const width = 100;
+const height = 200;
+const color = "red";
+
+const rectangle = {
+  width,
+ height,
+  color: color,
+  calculateArea() {
+    return this.width * this.height;
+  },
+  describe() {
+    return `Rectangle ${this.width}x${this.height}, color: ${this.color}`;
+  },
+};
