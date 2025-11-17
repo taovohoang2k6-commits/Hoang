f7e4d50fba6ae9021bf0714a1d0584acf18e729e
Hoang <taovohoang2k6@gmail.com>
2025-11-17 11:57:31 +0700

    asm

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Footer from "./components/Footer";
+import Header from "./components/Header"
 function App() {
-  const [count, setCount] = useState(0)
+const tours =[
+     {
+      id: 1,
+      title: 'Tour Du Lịch Cuba',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+     {
+      id: 2,
+      title: 'Tour Du Lịch Cuba',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+     {
+      id: 3,
+      title: 'Tour Du Lịch Cuba',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
 
+];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <><div>
+      <Header />
+      <h1 className="text-3x1 text-blue-500 text-center font-bold border-b-2 border-b-orange-300 uppercase">Tour nội địa</h1>
+      <p className="text-xl font-medium my-2">
+        Hãy tận hưởng trải nghiệm du lịch chuyên nghiệp, mang lại cho bạn những khoảnh khắc tuyệt vời và nâng tầm cuộc sống. Chúng tôi cam kết mang đến những chuyến đi đáng nhớ, giúp bạn khám phá thế giới theo cách hoàn hảo nhất
       </p>
-    </>
+      <div  className="flex gap-2">
+{tours.map(tour => (
+  <div>
+    <img src={tour.image} width={500} alt="" height={300} />
+    <h2 className="text-3xl font-bold text-orange-300">{tour.title}</h2>
+  </div>
+  
+))}
+      </div>
+       <Footer />
+    </div>
+   
+      
+      </>
   )
 }
 
---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,43 @@
+function Header() {
+  return (
+    <nav className="bg-white shadow-sm py-3 px-6 rounded-2xl">
+      <ul className="flex items-center gap-6 text-gray-700 font-medium">
+        <li>
+          <a
+            href="#"
+            className="hover:text-blue-600 transition-colors duration-200"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="hover:text-blue-600 transition-colors duration-200"
+          >
+            Home1
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="hover:text-blue-600 transition-colors duration-200"
+          >
+            Home2
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="hover:text-blue-600 transition-colors duration-200"
+          >
+            Home3
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
+    
